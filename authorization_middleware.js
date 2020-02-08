332d0582eb29115b1a5c8ceec90712caf5ea2379
Maxime Moreillon <m.moreillon@gmail.com>
2020-02-08 06:50:45 +0000

    testing CI pipeline

--- authorization_middleware.js
+++ authorization_middleware.js
@@ -4,6 +4,7 @@ function get_token(authorization_header){
   return authorization_header.split(" ")[1]
 }
 
+// secret set by the application
 exports.secret = undefined
 
 exports.middleware = (req, res, next) => {
@@ -15,36 +16,27 @@ exports.middleware = (req, res, next) => {
     // check if the secret is defined
     if(exports.secret){
       let token = get_token(req.headers.authorization);
-
       jwt.verify(token, exports.secret, (err, decoded) => {
-        if(decoded) {
-          // Simply allow anyone with a valid token
-          next();
-        }
-        else {
-          res.status(401)
-        }
+        if(err) res.status(500).send('Error decoding token')
+        if(decoded) next(); // Simply allow any user with a valid token
+        else res.status(401).send('Token is invalid')
       })
     }
     else {
-      console.log(new Error("secret is not set"))
-      res.status(500)
+      console.log(new Error("JWT secret is not set"))
+      res.status(500).send('JWT secret is not set in the server-side application')
     }
   }
 
-  // if using cookie-session
+  // Authorization using cookieSession
   else if('session' in req){
-    if('username' in req.session) {
-      next();
-    }
-    else {
-      res.status(401)
-    }
+    if('username' in req.session) next();
+    else res.status(401).('Appropriate session variable is not set')
   }
 
   else {
     // The client is not providing anything to authenticate
-    res.status(401)
+    res.status(403).send('This route requires authentication')
   }
 
 }
