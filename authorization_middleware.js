e1a1e70a29eccd02ac90ac55b5fec67c691a3db5
Maxime Moreillon <m.moreillon@gmail.com>
2020-02-12 11:06:23 +0000

    improved checks

--- authorization_middleware.js
+++ authorization_middleware.js
@@ -1,8 +1,5 @@
 const jwt = require('jsonwebtoken')
 
-function get_token(authorization_header){
-  return authorization_header.split(" ")[1]
-}
 
 // secret set by the application
 exports.secret = undefined
@@ -10,33 +7,34 @@ exports.secret = undefined
 exports.middleware = (req, res, next) => {
 
   // Authorization using a JWT
-  if('authorization' in req.headers) {
-    // Authorization header is set
+  if(req.headers.authorization) {
 
     // check if the secret is defined
-    if(exports.secret){
-      let token = get_token(req.headers.authorization);
-      jwt.verify(token, exports.secret, (err, decoded) => {
-        if(err) res.status(500).send('Error decoding token')
-        if(decoded) next(); // Simply allow any user with a valid token
-        else res.status(401).send('Token is invalid')
-      })
-    }
-    else {
+    if(!exports.secret){
       console.log(new Error("JWT secret is not set"))
       res.status(500).send('JWT secret is not set in the server-side application')
     }
+
+    // parse the headers to get the token
+    let token = req.headers.authorization.split(" ")[1];
+
+    // verify the token
+    jwt.verify(token, exports.secret, (err, decoded) => {
+      if(err) res.status(500).send('Error decoding token')
+
+      if(decoded) next(); // Simply allow any user with a valid token
+      else res.status(401).send('Token is invalid')
+    })
+
   }
 
   // Authorization using cookieSession
-  else if('session' in req){
-    if('username' in req.session) next();
+  else if(req.session){
+    if(req.session.username) next()
     else res.status(401).send('Appropriate session variable is not set')
   }
 
-  else {
-    // The client is not providing anything to authenticate
-    res.status(403).send('This route requires authentication')
-  }
+  // The client is not providing anything to authenticate
+  else res.status(403).send('This route requires authentication')
 
 }
